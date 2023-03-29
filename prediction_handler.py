e534f10f1dfd1d69236829c731881cea599e21ca
rodinKaradeniz <mrodin.karadeniz@mail.utoronto.ca>
2023-03-29 18:13:13 -0400

    minor changes with load_from_sql()

--- prediction_handler.py
+++ prediction_handler.py
@@ -1,20 +1,27 @@
+import os
+from data_processing.data_scraper import *
+from data_processing.data_processor import *
+from sentiment_analysis.train import predict
+
 def get_sentiment_prediction(ticker):
     # TODO: Implement
 
     # Get data from DB
+    # TODO: Introduce DB into this system
 
-    # Preprocess data
-
-    # Train model if not trained already
+    # Get relevant articles / tweets
+    # TODO: Twitter API
+    # TODO: Scrape relevant news
 
     # Predict
-
+    # TODO: Predict tweets using predict()
     pass
 
 def get_price_prediction(ticker):
     # TODO: Implement
 
     # Get data from DB
+    load_from_sql(ticker, debug=False)
 
     # Preprocess data
 
